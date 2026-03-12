ec35101f0ce99b96dc6b1876b1c98acfcf4116e3
aasthasoni7082-collab <aasthasoni7082@gmail.com>
2026-03-12 16:08:05 +0530

    mera TestPsaApplication.java

--- src/main/java/in_psa/TestPsaApplication.java
+++ src/main/java/in_psa/TestPsaApplication.java
@@ -8,6 +8,8 @@ public class TestPsaApplication {
 int x= 100;
 	public static void main(String[] args) {
 int x=100;
+		int z=100;
+		iny g=789;
 		SpringApplication.run(TestPsaApplication.class, args);
 	}
 
